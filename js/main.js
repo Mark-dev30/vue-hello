6142774064d197acface65ca0a64d9287182448f
Mark-dev30 <fronterremarco@gmail.com>
2022-12-16 14:45:21 +0100

    completed bonus

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Ciao Mondo!'
+        message: 'Ciao Mondo!',
+        insert_image: "https://images.unsplash.com/2/06.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
       }
     }
   }).mount('#app')
